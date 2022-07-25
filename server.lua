bce8acda11f2c0488960a1277e44ae6f7ed3db5c
Tyler Twisle <102629991+TwisleOffical@users.noreply.github.com>
2022-07-25 02:18:57 -0400

    Oops... Fixed

--- server.lua
+++ server.lua
@@ -5,10 +5,7 @@
 -- DO NOT EDIT IF YOU DONT KNOW WHAT YOU ARE DOING! --
 -- ONLY EDIT THE CONFIG.LUA FILE -- 
 
-RegisterServerEvent('TStoreRobber:server:Alert')
-AddEventHandler('TStoreRobber:server:Alert', function()
-    local _source = source
-    for i = 1, #Config.Stores, 1 do
-        TriggerClientEvent('chatMessage', -1, '^7[^124/7 Store Alarm^7]^2', {0,0,0} , Config.Stores[i].Name .. " Its Being Robbed")
-    end
+RegisterNetEvent('TStoreRobber:server:Alert')
+AddEventHandler('TStoreRobber:server:Alert', function(msg)
+    TriggerClientEvent('chatMessage', -1, msg)
 end)
