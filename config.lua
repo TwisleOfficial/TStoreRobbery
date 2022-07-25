f85998b122fb2a23252a3d5c8c757810b2fab531
Tyler Twisle <102629991+TwisleOffical@users.noreply.github.com>
2022-07-25 02:18:33 -0400

    Fixer Uper

--- config.lua
+++ config.lua
@@ -1,17 +1,18 @@
-Config = {}
+Config = {
 
-Config.lootTime = 10000 -- In Milliseconds / If You Dont Want A Loot Time Set It To 0
-Config.coolDown = 10000 -- In Milliseconds / If You Dont Want A Cooldown Set It To 0
+lootTime = 10000, -- In Milliseconds / If You Dont Want A Loot Time Set It To 0
+coolDown = 10000, -- In Milliseconds / If You Dont Want A Cooldown Set It To 0
 
-Config.Stores = { --{['Name'] = "", ['x'] = , ['y'] = , ['z'] = }, Template
-    {['Name'] = "Sandy", ['x'] = 1960.33, ['y'] = 3742.11, ['z'] = 32.43},
-    {['Name'] = "North LS", ['x'] = 374.19, ['y'] = 327.68, ['z'] = 103.57},
-    {['Name'] = "Senora Freeway", ['x'] = 2676.56, ['y'] = 3281.59, ['z'] = 55.24},
-    {['Name'] = "Grape Seed", ['x'] = 1698.33, ['y'] = 4924.26, ['z'] = 42.06},
-    {['Name'] = "Chumash", ['x'] = -3243.25, ['y'] = 1001.35, ['z'] = 12.83},
-    {['Name'] = "Mirror Park", ['x'] = 1163.44, ['y'] = -323.86, ['z'] = 69.21},
-    {['Name'] = "Grove Street", ['x'] = -48.05, ['y'] = -1757.48, ['z'] = 29.42},
-    {['Name'] = "Little Seoul", ['x'] = -707.41, ['y'] = -913.95, ['z'] = 19.22},
-    {['Name'] = "Palomino Freeway", ['x'] = 2556.3, ['y'] = 382.13, ['z'] = 108.62},
-    {['Name'] = "Harmony / Route 68", ['x'] = 548.14, ['y'] = 2668.62, ['z'] = 42.16},
+Stores = { --{Name = "", Alert = "", x = , y = , z = }, Template
+    {Name = "Sandy", Alert = "[^1Silent Alarm^0] ^3Sandy ^024/7 Is Being ^1Robbed", x = 1960.33, y = 3742.11, z = 32.43},
+    {Name = "North LS", Alert = "[^1Silent Alarm^0] ^3North LS ^024/7 Is Being ^1Robbed", x = 374.19, y = 327.68, z = 103.57},
+    {Name = "Senora Freeway", Alert = "[^1Silent Alarm^0] ^3Senora Freeway ^024/7 Is Being ^1Robbed", x = 2676.56, y = 3281.59, z = 55.24},
+    {Name = "Grape Seed", Alert = "[^1Silent Alarm^0] ^3Grape Seed ^024/7 Is Being ^1Robbed", x = 1698.33, y = 4924.26, z = 42.06},
+    {Name = "Chumash", Alert = "[^1Silent Alarm^0] ^3Chumash ^024/7 Is Being ^1Robbed", x = -3243.25, y = 1001.35, z = 12.83},
+    {Name = "Mirror Park", Alert = "[^1Silent Alarm^0] ^3Mirror Park ^024/7 Is Being ^1Robbed", x = 1163.44, y = -323.86, z = 69.21},
+    {Name = "Grove Street", Alert = "[^1Silent Alarm^0] ^3Grove Street ^024/7 Is Being ^1Robbed", x = -48.05, y = -1757.48, z = 29.42},
+    {Name = "Little Seoul", Alert = "[^1Silent Alarm^0] ^3Little Seoul ^024/7 Is Being ^1Robbed", x = -707.41, y = -913.95, z = 19.22},
+    {Name = "Palomino Freeway", Alert = "[^1Silent Alarm^0] ^3Palomino Freeway ^024/7 Is Being ^1Robbed", x = 2556.3, y = 382.13, z = 108.62},
+    {Name = "Harmony / Route 68", Alert = "[^1Silent Alarm^0] ^3Harmony / Route 68 ^024/7 Is Being ^1Robbed", x = 548.14, y = 2668.62, z = 42.16},
+}
 }
